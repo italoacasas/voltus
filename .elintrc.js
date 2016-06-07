854c61f6dd816f31793854ca3105ea90456fd10f
Italo A. Casas <me@italoacasas.com>
2016-06-07 14:42:33 -0400

    adding a better eslint implementation

--- .elintrc.js
+++ .elintrc.js
@@ -1,26 +1,3 @@
-module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "rules": {
-        "indent": [
-            2,
-            4,
-            {"SwitchCase": 1}
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
-};
+{
+  "extends": "@flexshopper/flexshopper/es6"
+}
