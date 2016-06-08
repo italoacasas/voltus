b8bb7fd3555400d397e2d77d6e76480ed89fe757
Italo A. Casas <me@italoacasas.com>
2016-06-08 14:06:08 -0400

    adding phone tests

---
+++ test/phone.js
@@ -0,0 +1,12 @@
+const expect = require('chai').expect;
+const phone = require('../lib/index.js').phone;
+
+describe('Phone', () => {
+
+    it('should return true', () => {
+        expect(phone('305-305-3056')).to.be.true
+    });
+    it('should return false', () => {
+        expect(phone('yu-gi-oh')).to.be.false;
+    });
+});
